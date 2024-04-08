407799e59d7efe7f07da2334400dea6772b960dc
KondratenkoS <sergeikhell@gmail.com>
2024-04-08 22:44:07 +0300

    adding assets and IndexController with rout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group('/', function () {
-    return 'aaaaaaaaaa';
+Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
+    Route::get('/', 'IndexController');
 });
 
 Auth::routes();
