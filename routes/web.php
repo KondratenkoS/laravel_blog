c9dbad5736aa46b34e3c677185ea2086206a5042
KondratenkoS <sergeikhell@gmail.com>
2024-07-23 18:48:03 +0300

    output to main page

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
-    Route::get('/', 'IndexController');
+    Route::get('/', 'IndexController')->name('main.index');
 });
 
 
